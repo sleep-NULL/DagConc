250e54e06e59356684b67120266cb79bf41fd60d
sleep-NULL <sleep.hyf@gmail.com>
2019-06-21 21:01:05 +0800

    Add chaining for graph

--- core/src/main/java/com/github/dag/core/graph/Graph.java
+++ core/src/main/java/com/github/dag/core/graph/Graph.java
@@ -3,6 +3,7 @@ package com.github.dag.core.graph;
 import lombok.Data;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by huangyafeng on 2019/6/6.
@@ -90,8 +91,71 @@ public class Graph<T> {
      * @return
      */
     public Graph<List<T>> chaining() {
-        // TODO
-        return null;
+        int[] inDegreeCopy = new int[inDegree.length];
+        System.arraycopy(inDegree, 0, inDegreeCopy, 0, inDegree.length);
+        Deque<Integer> queue = new LinkedList();
+        queue.addAll(zeroDegreeIdx);
+        // 新的入度为 0 的节点表
+        List<Integer> newZeroDegreeIndex = new ArrayList<>();
+        // 新的入度表
+        List<Integer> newInDegree = new ArrayList<>();
+        // 新的节点表
+        List<List<T>> newNodes = new ArrayList<>();
+        // 新的邻接表
+        List<List<Integer>> newAdjacencyList = new ArrayList<>();
+        int stageIndex = 0;
+        // 当前 stage
+        List<T> currentStage;
+        // 原图 node index -> 新图 stage 索引
+        Map<Integer, Integer> nodeIndexStageMap = new HashMap<>();
+        while (!queue.isEmpty()) {
+            currentStage = new ArrayList<>();
+            Integer i = queue.removeFirst();
+            newNodes.add(currentStage);
+            currentStage.add(nodes[i]);
+            newInDegree.add(inDegree[i]);
+            if (inDegree[i] == 0) {
+                newZeroDegreeIndex.add(stageIndex);
+            }
+            nodeIndexStageMap.put(i, stageIndex);
+            stageIndex++;
+            // 邻接表为空, 无后继节点，单独作为一个 stage
+            if (adjacencyList[i] == null || adjacencyList[i].isEmpty()) {
+                newAdjacencyList.add(new ArrayList<>());
+                continue;
+            }
+            // 后继节点 size 为 1, 且后继节点入度为 1, 合并到当前 stage
+            int j = i;
+            while (adjacencyList[j] != null && adjacencyList[j].size() == 1 && (inDegree[adjacencyList[j].get(0)] == 1)) {
+                j = adjacencyList[j].get(0);
+                currentStage.add(nodes[j]);
+            }
+            if (adjacencyList[j] != null) {
+                for (Integer next : adjacencyList[j]) {
+                    if (--inDegreeCopy[next] == 0) {
+                        queue.add(next);
+                    }
+                }
+            }
+            newAdjacencyList.add(adjacencyList[j] == null ? new ArrayList<>() : adjacencyList[j]);
+        }
+        for (int i = 0; i < newAdjacencyList.size(); i++) {
+            List<Integer> nextList = newAdjacencyList.get(i);
+            newAdjacencyList.set(i, nextList.stream().map(idx -> nodeIndexStageMap.get(idx)).collect(Collectors.toList()));
+        }
+        int[] newInDegreeArr = new int[newInDegree.size()];
+        for (int i = 0; i < newInDegree.size(); i++) {
+            newInDegreeArr[i] = newInDegree.get(i);
+        }
+        List<T>[] newNodesArr = new List[newNodes.size()];
+        for (int i = 0; i < newNodes.size(); i++) {
+            newNodesArr[i] = newNodes.get(i);
+        }
+        List<Integer>[] newAdjacencyListArr = new List[newAdjacencyList.size()];
+        for (int i = 0; i < newAdjacencyList.size(); i++) {
+            newAdjacencyListArr[i] = newAdjacencyList.get(i);
+        }
+        return new Graph<>(newNodesArr, newInDegreeArr, newAdjacencyListArr, newZeroDegreeIndex);
     }
 
     /**
--- core/src/test/java/com/github/dag/core/ConcExecuteTest.java
+++ core/src/test/java/com/github/dag/core/ConcExecuteTest.java
@@ -4,6 +4,7 @@ import com.github.dag.core.graph.Graph;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -19,6 +20,14 @@ public class ConcExecuteTest {
         public Node(String name, int age) {
             this.name = name;
             this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
         }
     }
 
@@ -102,6 +111,63 @@ public class ConcExecuteTest {
                 .build();
 
         Assert.assertEquals(graph.isDAG(), false);
+    }
+
+    @Test
+    public void test3() {
+        Node[] nodes = new Node[]{
+                new Node("test0", 0),
+                new Node("test1", 1),
+                new Node("test2", 2),
+                new Node("test3", 3),
+                new Node("test4", 4),
+                new Node("test5", 5),
+                new Node("test6", 6),
+                new Node("test7", 7),
+        };
+        Graph<List<Node>> graph = Graph.builder()
+                .addNodes(nodes)
+                .addEdge(nodes[0], nodes[1])
+                .addEdge(nodes[1], nodes[2])
+                .addEdge(nodes[2], nodes[3])
+                .addEdge(nodes[2], nodes[4])
+                .addEdge(nodes[3], nodes[6])
+                .addEdge(nodes[4], nodes[7])
+                .build().chaining();
+
+        System.out.println(graph);
+
+        ExecutorService executorService = getExecutorService(2);
+        new ConcExecute<>(graph, executorService).executeSync((ns, submitTime) -> {
+            for (Node node : ns) {
+                System.out.println(node.name + " : " + node.age);
+            }
+        });
+    }
+
+    @Test
+    public void test4() {
+        Node[] nodes = new Node[]{
+                new Node("test0", 0),
+                new Node("test1", 1),
+                new Node("test2", 2),
+                new Node("test3", 3),
+                new Node("test4", 4),
+        };
+        Graph<List<Node>> graph = Graph.builder()
+                .addNodes(nodes)
+                .addEdge(nodes[0], nodes[1])
+                .addEdge(nodes[1], nodes[2])
+                .addEdge(nodes[2], nodes[3])
+                .addEdge(nodes[3], nodes[4])
+                .build().chaining();
+
+        ExecutorService executorService = getExecutorService(2);
+        new ConcExecute<>(graph, executorService).executeSync((ns, submitTime) -> {
+            for (Node node : ns) {
+                System.out.println(node.name + " : " + node.age);
+            }
+        });
     }
 
     private ExecutorService getExecutorService(int i) {
